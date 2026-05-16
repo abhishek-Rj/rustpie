a99fd9836b39827c061965acaf49da6f3bdb8e0b
abhishek-Rj <mahitomaki96@gmail.com>
2026-05-16 20:03:55 +0530

    reading chunks of bytes from kernel tcp conn buffer.

---
+++ src/buffer/methods.rs
@@ -0,0 +1,11 @@
+pub struct Buffer {
+    data: [u8; 5],
+    start: usize,
+    end: usize,
+}
+
+impl Buffer {
+    pub fn readable() {}
+
+    pub fn 
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+mod buffer;
+
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+
+#[allow(unused)]
+struct Buffer {
+    data: [u8; 6],
+    start: usize,
+    end: usize,
+}
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = Buffer {data: [0u8; 6], start: 0, end: 0};
+    
+    loop {
+        let buff_read = stream.read(&mut buffer).unwrap();
+        if buff_read == 0 {
+            return
+        }
+
+        println!(
+            "Read {} bytes: {:?}",
+            buff_read,
+            &buffer[..buff_read]
+        );
+
+        println!(
+            "As string: {}",
+            String::from_utf8_lossy(&buffer[..buff_read])
+        );
+    }
 }
