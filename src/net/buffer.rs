149c993ef25134d8b41f1daef0a7ab3068a36fbd
abhishek-Rj <mahitomaki96@gmail.com>
2026-05-29 01:10:48 +0530

    compact buffer

--- src/net/buffer.rs
+++ src/net/buffer.rs
@@ -2,28 +2,29 @@
 
 #[derive(Default)]
 pub struct Buffer {
-    data: Vec<u8>,
-    read_pos: usize,
-    write_pos: usize,
+    pub data: Vec<u8>,
+    pub read_pos: usize,
+    pub write_pos: usize,
 }
 
 impl Buffer {
     pub fn new() -> Self {
         return Buffer {
-            data: Vec::with_capacity(1024),
+            data: Vec::with_capacity(512),
             ..Default::default()
         };
     }
 
-    pub fn write_bytes(&mut self, chunk: &mut [u8]) -> () {
+    pub fn write_bytes(&mut self, chunk: &[u8]) -> () {
         self.data.extend_from_slice(chunk);
         self.write_pos += chunk.len();
     }
 
     pub fn readable_slice(&self) -> &[u8] {
-        return &self.data[self.read_pos..self.write_pos]
+        return &self.data[self.read_pos..self.write_pos];
     }
 
+    //NO actual use just for learning a compact buffer
     pub fn consume(&mut self, n: usize) {
         self.read_pos += n;
     }
